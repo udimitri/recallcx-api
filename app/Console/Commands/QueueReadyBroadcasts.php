402e6b00cade5999ddb477a66971d1dfb7566e06
Dimitri Trofimuk <dimitri.trofimuk@ualberta.ca>
2024-07-25 18:36:54 -0600

    .

--- app/Console/Commands/QueueReadyBroadcasts.php
+++ app/Console/Commands/QueueReadyBroadcasts.php
@@ -26,16 +26,11 @@ class QueueReadyBroadcasts extends Command
             $this->info("Found {$targets->count()} broadcasts(s) ready for sending!");
 
             foreach ($targets as $target) {
-//                $business = $target->business;
-//
-//                $contacts = $business->contacts()
-//                    ->whereNull('unsubscribed_at');
-//
-//                $contacts->chunk(100, function ($contacts) use ($target) {
-//                    foreach ($contacts as $contact) {
-//                        // dispatch(new SendBroadcast($target, $contact))->afterCommit();
-//                    }
-//                });
+                $target->audience()->chunk(100, function ($contacts) use ($target) {
+                    foreach ($contacts as $contact) {
+                        dispatch(new SendBroadcast($target, $contact))->afterCommit();
+                    }
+                });
 
                 $target->status = BroadcastStatus::Sending;
                 $target->save();
